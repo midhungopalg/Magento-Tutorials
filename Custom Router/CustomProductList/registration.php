929a9155d56f16cd26e1609f233bf7bdb78cc03d
Midhun <midhungopalg@gmail.com>
2022-12-10 22:08:29 +0530

    custom router

---
+++ Custom Router/CustomProductList/registration.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @package Midhun_CustomProductList
+ * @author  Midhun
+ */
+declare(strict_types=1);
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+    ComponentRegistrar::MODULE,
+    'Midhun_CustomProductList',
+    __DIR__
+);
